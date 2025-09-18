15c83c2305b5c5a97683fc8b03dee05f2dc59076
C-Vieira <caiovieira.official@gmail.com>
2025-09-18 16:41:25 -0300

    feat: score UI display

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
 
 public class GameState : MonoBehaviour
 {
+    public Text scoreText;
     private static float score = 0.0f;
+
+    void Start()
+    {
+        // Reset score on start
+        score = 0.0f;
+    }
+
+    void Update()
+    {
+        // Update score visuals
+        DisplayScore();
+    }
 
     public static void UpdateScore(float value)
     {
         score += value;
-        print($"Score: {score}");
+    }
+
+    private void DisplayScore()
+    {
+        scoreText.text = "Score: " + score.ToString("0");
     }
 }
